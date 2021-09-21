df3df9bcf1bcf79a9ea84f4d4479f5c46a9e944d
Sam Macbeth <sam@macbeth.cc>
2021-09-21 18:41:39 +0200

    sams-web-tweaks v1

---
+++ sams-web-tweaks/content-scripts/theguardian.js
@@ -0,0 +1,19 @@
+
+function runConsent() {
+    const manageButton = document.querySelector('button[title="Manage my cookies"')
+    const rejectButton = document.querySelector('button[title="Reject all"]');
+    if (manageButton !== null) {
+        manageButton.click()
+        return true;
+    } else if (rejectButton !== null) {
+        rejectButton.click()
+        return true;
+    }
+    return false;
+}
+
+const checkInterval = setInterval(() => {
+    if (runConsent()) {
+        clearInterval(checkInterval)
+    } 
+}, 50)
